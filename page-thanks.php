26f501e16effc835a7f3604c1ba9d3db162be173
fumiya.tsuji <fumiya0903613@gmail.com>
2022-04-08 19:20:50 +0900

    送信完了画面追加

---
+++ page-thanks.php
@@ -0,0 +1,13 @@
+<html>
+  <?php get_template_part('head'); ?>
+  <body>
+    <div class="wrapper">
+      <?php get_header("contact"); ?>
+      <main>
+        <?php get_template_part('parts/thanks'); ?>
+      </main>
+      <?php get_template_part('script'); ?>
+      <?php get_footer(); ?>
+    </div>
+  </body>
+</html>
