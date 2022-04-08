ba1f843de331cb33b14b45064e7913d184bb2e11
fumiya.tsuji <fumiya0903613@gmail.com>
2022-04-08 18:04:14 +0900

    サイトについての文章作成

--- parts/about.php
+++ parts/about.php
@@ -5,11 +5,22 @@
       <h3 class="h3-title">辻 郁矢／Webエンジニア</h3>
       <div class="about-text">
         <h4>経歴</h4>
-        <div>・2019年</div>
+        <div>・2015〜2018年 専門学校にてプログラミング言語や基本情報技術者などを学び、習得</div>
+        <div>・2018〜2021年 ソフトウェア開発会社に就職</div>
+        <div>・2021年〜現在 フリーランスエンジニアに転身</div>
         <h4>活動拠点</h4>
         <div>東京(出身は奈良)</div>
+        <h4>資格</h4>
+        <div>基本情報技術者</div>
+        <div>ITパスポート</div>
+        <div>Javaプログラミング認定試験1級</div>
+        <div>MicroSoft Office Specialist Word 2010</div>
+        <div>MicroSoft Office Specialist Excel 2010</div>
         <h4>メッセージ</h4>
-        <div>今までの経験</div>
+        <div>
+          JavaやRuby、VueやReact、Wordpressバックエンドとフロントエンド問わず現場でスキルを身につけてきました。<br>また、学生時代に様々な資格を取得もしてきました。
+          今までの経験を活かして、お客様のご満足頂けるWeb制作・<br>開発を行えるよう尽力させて頂きます。
+        </div>
       </div>
     </div>
   </div>
